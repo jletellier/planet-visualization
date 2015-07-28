2f3a454b2b786cedfe70bb92a8c7bbe305343ac2
Julien Letellier <letellier.julien@gmail.com>
2015-07-28 10:55:06 +0200

    Added new helper to get a 2d context quickly

--- src/helper.js
+++ src/helper.js
@@ -18,7 +18,29 @@ function lonLatToWorldCoord(longitude, latitude) {
     return { x: x, y: y, z: z };
 }
 
+function createContext2D(width, height, insertDom) {
+    var canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+
+    if (insertDom) {
+        canvas.style.position = 'absolute';
+        canvas.style.top = '0';
+        canvas.style.left = '0';
+        canvas.style.margin = '0';
+        canvas.style.padding = '0';
+        canvas.style.width = canvas.width + 'px';
+        canvas.style.height = canvas.height + 'px';
+
+        document.body.appendChild(canvas);
+    }
+
+    var ctx = canvas.getContext('2d');
+    return ctx;
+}
+
 module.exports = {
     lonLatToTexCoord: lonLatToTexCoord,
-    lonLatToWorldCoord: lonLatToWorldCoord
+    lonLatToWorldCoord: lonLatToWorldCoord,
+    createContext2D: createContext2D
 };
